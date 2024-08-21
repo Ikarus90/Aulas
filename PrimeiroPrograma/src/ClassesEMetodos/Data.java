90ac5d3948b5ecd0038febe586eca539af786a26
Ikarus90 <ikarusgamer90@gmail.com>
2024-08-21 03:48:42 -0300

    2

--- PrimeiroPrograma/src/ClassesEMetodos/Data.java
+++ PrimeiroPrograma/src/ClassesEMetodos/Data.java
@@ -7,22 +7,31 @@ public class Data {
 	int ano;
 	
 Data(){
-	dia = 1;
-	mes = 1;
-	ano = 1970;
-		
+	//dia = 1;
+	//mes = 1;
+	//ano = 1970;
+	
+	this(1, 1, 1970);
+	
+	// byte, short, int, long -> 0
+	// float, double -> 0.0
+	// boolean-> false
+	// char -> '\u0000'
+	// o valor padão só é definido quando não é definido nenhuma variavel de classe
+	// objetos -> null
+	
+	
 	}
 	
-	Data(int diaInicial, int mesInicial, int anoInicial){
-		dia = diaInicial;
-		mes = mesInicial;
-		ano = anoInicial;
+	Data(int dia, int mes, int ano){
+		this.dia = dia;
+		this.mes = mes;
+		this.ano = ano;
 	}	
-	
-	
+
 	String obterDataFormatada() {
-		return String.format("%d/%d/%d", dia, mes, ano);
-		
+		String formato = "%d/%d/%d";
+		return String.format(formato, dia, mes, ano);		
 	}
 	
 	void imprimirDataFormatada() {
